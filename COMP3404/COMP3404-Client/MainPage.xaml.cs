e7926ce2ff27bd3d54a91a1abdce621ad94c3e23
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2025-03-31 16:48:05 +0100

    some comments

--- COMP3404/COMP3404-Client/MainPage.xaml.cs
+++ COMP3404/COMP3404-Client/MainPage.xaml.cs
@@ -43,7 +43,8 @@ public partial class MainPage : ContentPage
 #pragma warning restore CA1416
 
 		// THIS WORKS UP TO HERE
-		// todo: change redirect url on github to point to server URL
+		// todo: change redirect url on github to point to server URL? i think browser should just redirect to server auth endpoint and then server redirects back to app? maybe?
+		// alternatively i could just ping the API with the code and such from here that would work
 
 		//string accessToken = authResult.AccessToken;
 		//m_nameBox.Text = accessToken;
