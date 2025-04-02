ee4822298af688e578ac1ba3f5d602ec75b4f306
Lauren Frankland <fral3_21@uni.worc.ac.uk>
2025-04-02 17:22:58 +0100

    settings page

---
+++ COMP3404/COMP3404-Client/SettingsPage.xaml.cs
@@ -0,0 +1,15 @@
+namespace COMP3404_Client;
+
+public partial class SettingsPage : ContentPage
+{
+	public SettingsPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void OnHomeButtonClicked(object sender, EventArgs e)
+    {
+        // settings page nav with shell
+        await Shell.Current.GoToAsync("///" + nameof(MainPage));
+    }
+}
