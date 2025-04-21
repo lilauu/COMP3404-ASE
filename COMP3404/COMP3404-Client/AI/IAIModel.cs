058fb52f7b3d313758cc1720e2c33b4cab2fc0d0
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2025-04-21 11:32:35 +0100

    add doc comment

--- COMP3404/COMP3404-Client/AI/IAIModel.cs
+++ COMP3404/COMP3404-Client/AI/IAIModel.cs
@@ -7,6 +7,12 @@ namespace COMP3404_Client.AI;
 /// </summary>
 public interface IAIModel
 {
+    /// <summary>
+    /// Gets a response from the AI model.
+    /// </summary>
+    /// <param name="message">The message that the user has sent</param>
+    /// <param name="conversation">The ongoing conversation, containing message history</param>
+    /// <param name="onResponseReceived">A callback that is called when a response is received from the model, with a string parameter of the model's response</param>
     public void GetResponse(string message, Conversation conversation, Action<string> onResponseReceived);
 
 
