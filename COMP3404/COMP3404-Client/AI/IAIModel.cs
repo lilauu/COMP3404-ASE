b5353ffc881cb568831f999c0eca14990b4c6afc
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2025-04-21 11:02:57 +0100

    create initial interface and implementation

---
+++ COMP3404/COMP3404-Client/AI/IAIModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3404_Client.AI;
+
+/// <summary>
+/// The interface that UI etc. should use when interacting with the AI model.
+/// </summary>
+public interface IAIModel
+{
+
+}
---
+++ COMP3404/COMP3404-Client/AI/Stub/StubAIModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3404_Client.AI.Stub;
+
+internal class StubAIModel : IAIModel
+{
+
+}
