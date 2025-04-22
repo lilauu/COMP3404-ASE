dfe4e76629875bf4e1b3e0f921f015d6b2a192e1
ElliotAubrey <eaubrey1999@gmail.com>
2025-04-22 10:33:55 +0100

    Added new interface (IChatPersistence)

---
+++ COMP3404/COMP3404-Client/IChatPersistence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3404_Client
+{
+    internal interface IChatPersistence
+    {
+        void SaveChat();
+        void LoadChat();
+    }
+}
