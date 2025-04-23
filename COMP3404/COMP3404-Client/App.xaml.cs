5dd88bd14860ba811e62a7bf8e2fe4a65be18459
aube1_18 <aube1_18@CH1007-25>
2025-04-23 12:19:36 +0100

    Revert app.xaml.cs

--- COMP3404/COMP3404-Client/App.xaml.cs
+++ COMP3404/COMP3404-Client/App.xaml.cs
@@ -7,12 +7,6 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
-#if (WINDOWS)
-        //Create a new TTS class to assign the singleton referance
-        TTS tts = new(new TTSSettings(true, 0, 100,
-        System.Speech.Synthesis.VoiceGender.Male, System.Speech.Synthesis.VoiceAge.Adult));
-
-#endif
 
         MainPage = new AppShell();
     }
