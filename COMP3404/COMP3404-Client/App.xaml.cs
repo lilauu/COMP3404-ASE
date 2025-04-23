7decb037304b1d9cf852918a6d5a2f5eb2989672
aube1_18 <aube1_18@CH1007-25>
2025-04-23 12:16:16 +0100

    Revert accidental changes

--- COMP3404/COMP3404-Client/App.xaml.cs
+++ COMP3404/COMP3404-Client/App.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace COMP3404_Client;
+﻿namespace COMP3404_Client;
 
 public partial class App : Application
 {
     public App()
     {
         InitializeComponent();
-#if (WINDOWS)
-        //Create a new TTS class to assign the singleton referance
-        TTS tts = new(new TTSSettings(true, 0, 100,
-        System.Speech.Synthesis.VoiceGender.Male, System.Speech.Synthesis.VoiceAge.Adult));
-
-#endif
 
         MainPage = new AppShell();
     }
