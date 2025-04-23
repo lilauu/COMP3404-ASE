34d5e8ab412776b63b62f1d8c7c7ff25021d4925
aube1_18 <aube1_18@CH1007-25>
2025-04-23 13:16:41 +0100

    Added SetLanguage to IAIModel

--- COMP3404/COMP3404-Client/AI/Stub/StubAIModel.cs
+++ COMP3404/COMP3404-Client/AI/Stub/StubAIModel.cs
@@ -9,4 +9,9 @@ internal class StubAIModel : IAIModel
         // doesnt use the conversation at all
         onResponseReceived($"This is a stub response! You said: {message}");
     }
+
+    public void SetLanguage(string language, string message, Conversation conversation, Action<string> onResponseReceived)
+    {
+        onResponseReceived($"This is a stub response! You said: {message} and I converted it into {language}");
+    }
 }
