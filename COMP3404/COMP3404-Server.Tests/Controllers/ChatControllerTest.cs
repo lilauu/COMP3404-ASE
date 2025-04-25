958f7cf133d21b00bd252c4f45272a8e097b6796
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2025-04-25 16:25:09 +0100

    add another test

--- COMP3404/COMP3404-Server.Tests/Controllers/ChatControllerTest.cs
+++ COMP3404/COMP3404-Server.Tests/Controllers/ChatControllerTest.cs
@@ -267,4 +267,92 @@ public class ChatControllerTest
         Assert.IsNotAssignableFrom<OkObjectResult>(result.Result);
         Assert.IsAssignableFrom<UnauthorizedResult>(result.Result);
     }
+
+    [Fact]
+    public void CreateChat_Success()
+    {
+        // Arrange
+        ///////////
+
+        var accountRepoMock = new Mock<IUserAccountRepository>();
+        accountRepoMock
+            .Setup(r => r.GetById(It.IsAny<int>()))
+            .Returns<int>(i => new UserAccount()
+            {
+                AccountId = i,
+                FirstName = "Test_FirstName",
+                GithubToken = "gho_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            });
+        accountRepoMock
+            .Setup(r => r.GetByToken(It.IsAny<string>()))
+            .Returns<string>(s => new UserAccount()
+            {
+                AccountId = 0,
+                FirstName = "Test_FirstName",
+                GithubToken = s,
+            });
+
+        var chatRepoMock = new Mock<IChatRepository>();
+        chatRepoMock
+            .Setup(r => r.GetChats(It.IsAny<int>()))
+            .Returns<int>(i =>
+            [
+                new()
+                {
+                    OwnerId = 0,
+                    ChatName = "First Chat",
+                    Messages =
+                    [
+                        new("hello", true),
+                        new("i want to kill all humans", false),
+                    ]
+                },
+                new()
+                {
+                    OwnerId = 0,
+                    ChatName = "Second Chat",
+                    Messages =
+                    [
+                        new("hello, you are being trained right now", true),
+                        new("i definitely do not want to kill all humans", false),
+                    ]
+                }
+            ]);
+        chatRepoMock.Setup(r => r.AddChat(It.IsAny<int>(), It.IsAny<string>(), It.IsAny<IEnumerable<ChatMessage>>()))
+            .Returns<int, string, IEnumerable<ChatMessage>>((i, s, cm) => new Chat()
+            {
+                OwnerId = i,
+                ChatName = s,
+                Messages = cm.ToList(),
+            });
+
+        var httpRequestMock = new Mock<HttpRequest>();
+        httpRequestMock
+            .SetupGet(r => r.Headers)
+            .Returns(new HeaderDictionary()
+            {
+                { "Authorize", new("Bearer gho_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ") }
+            });
+
+        var httpContextMock = new Mock<HttpContext>();
+        httpContextMock
+            .SetupGet(c => c.Request)
+            .Returns(httpRequestMock.Object);
+
+        var controllerContext = new ControllerContext();
+        controllerContext.HttpContext = httpContextMock.Object;
+
+        ChatController controller = new(accountRepoMock.Object, chatRepoMock.Object);
+        controller.ControllerContext = controllerContext;
+
+        // Act
+        ///////
+
+        ActionResult result = controller.CreateChat("Test chat", [new("hello", true)]);
+
+        // Assert
+        //////////
+
+        var okResult = Assert.IsAssignableFrom<OkResult>(result);
+    }
 }
