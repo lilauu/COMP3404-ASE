e4f0fba9095d2680923e3b5fe91f8a534068d29e
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2025-04-26 17:37:12 +0100

    remove redundant code

--- COMP3404/COMP3404-Client/App.xaml.cs
+++ COMP3404/COMP3404-Client/App.xaml.cs
@@ -1,18 +1,10 @@
-﻿using COMP3404_Client.Services;
-
-namespace COMP3404_Client;
+﻿namespace COMP3404_Client;
 
 public partial class App : Application
 {
-    readonly HttpClient m_client;
-    readonly ServerService m_dataManager;
-
     public App()
     {
         InitializeComponent();
-
-        m_client = new HttpClient();
-        m_dataManager = new ServerService(m_client);
 
         MainPage = new AppShell();
     }
